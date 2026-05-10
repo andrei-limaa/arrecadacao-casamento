88c7c89b4af08ef6ea28406cfe8c1d686237b621
andrei-limaa <andreipereiralima55@gmail.com>
2026-05-09 23:23:11 -0300

    🚀 fix: configura rota resetar_pix e ajustes de produção

--- presentes/urls.py
+++ presentes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('lista/', views.lista, name='lista'),
     path('webhook/', views.webhook_mercadopago, name='webhook_mp'),
+     path('resetar/', views.resetar_pix, name='resetar_pix'),
 ]
